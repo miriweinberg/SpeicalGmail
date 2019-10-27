eda90e86d540f05cba371f5dd483e1e14ff1323d
miriweinberg <miri7675163@gmail.com>
2019-10-27 10:31:19 +0200

    first

---
+++ sendEmailAPI/sendEmailAPI/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace sendEmailAPI
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
